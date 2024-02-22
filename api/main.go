d5d75c40d447c29fe6ba30f0cb6603cf9b8fcc21
liendev <liens@alphaefficiency.me>
2024-02-22 02:34:35 -0300

    Backed first commit

--- api/main.go
+++ api/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-    fmt.Println("Hello, Go!")
+	// Define a handler function for your API endpoint
+	http.HandleFunc("/api/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello, Go API!\n")
+	})
+
+	// Start the HTTP server on port 8080
+	fmt.Println("Server is running on http://localhost:8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
 }
